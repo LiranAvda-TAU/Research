f266e6dcf8691b8f9c72e597298c8d6e62072896
LiranAvda-TAU <56253700+LiranAvda-TAU@users.noreply.github.com>
2020-02-06 12:35:10 +0200

    deleted unnecessary services

--- WEBAPP/RESEARCH_APP/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResearchappConfig(AppConfig):
-    name = 'RESEARCH_APP'
--- WEBAPP/RESEARCH_APP/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Gene
-
-class GeneSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Gene
-        fields = ('name', 'orthologs')
