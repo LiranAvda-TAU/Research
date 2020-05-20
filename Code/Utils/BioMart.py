60e99971af8cc5c280d27b2df85b1322cd0e8b2d
LiranAvda-TAU <56253700+LiranAvda-TAU@users.noreply.github.com>
2020-05-20 12:23:59 +0300

    changed the function from get id to get seq

--- Code/Utils/BioMart.py
+++ Code/Utils/BioMart.py
@@ -1,19 +1,17 @@
-import requests
 from pybiomart import Dataset
-
 from Code.Http.HttpRequester import HttpRequester
-from Code.Utils.Strings import Strings
+from Code.Utils.Ensembl import Ensembl
 
 
 class BioMart:
     def __init__(self):
         self.dataset = Dataset(name='hsapiens_gene_ensembl', host='http://www.ensembl.org')
+        self.df = self.dataset.query(attributes=['ensembl_gene_id', 'uniprotswissprot']).dropna()
 
-    def get_uniport_swissport_id(self, gene_id):
-        df = self.dataset.query(attributes=['ensembl_gene_id', 'uniprotswissprot']).dropna()
-        uni_ids = df.loc[df['Gene stable ID'] == gene_id]['UniProtKB/Swiss-Prot ID']
+    def get_swissprot_sequence_from_biomart(self, gene_id):
+        uni_ids = self.df.loc[self.df['Gene stable ID'] == gene_id]['UniProtKB/Swiss-Prot ID']
         if uni_ids.size == 1:
-            return uni_ids.iloc[0]
+            return HttpRequester.get_protein_seq_by_uniprot_swissprot_id(uni_ids.iloc[0])
         elif uni_ids.size == 0:
             return None
         else:  # many ids
@@ -24,20 +22,33 @@ class BioMart:
                 if uni_id not in reviewed_html:
                     lst.remove(uni_id)
             # choose by length
-            chosen_uni_id = None
             chosen_seq = ''
             for uni_id in lst:
-                try:
-                    r = requests.get("https://www.uniprot.org/uniprot/" + uni_id + ".fasta")
-                    response_body = r.text
-                except Exception as e:
-                    print("Problem in function get_uniport_swissport_id while trying to extract sequence for",
-                          uni_id, "in gene", gene_id, ":", e)
-                    return None
-                optional_seq = Strings.from_fasta_seq_to_seq(response_body)
+                optional_seq = HttpRequester.get_protein_seq_by_uniprot_swissprot_id(uni_id)
                 if len(optional_seq) > len(chosen_seq):
                     chosen_seq = optional_seq
-                    chosen_uni_id = uni_id
-            return chosen_uni_id
+            return chosen_seq
+
+            # access function
+
+    def get_human_protein_from_uniprot_by_gene_id(self, gene_id):
+        # first try canonical sequence
+        canonical_protein_sequence = self.get_swissprot_sequence_from_biomart(gene_id)
+        if canonical_protein_sequence:
+            return canonical_protein_sequence
+        else:
+            # get longest sequence
+            longest_seq = HttpRequester().get_longest_human_protein_sequence_from_uniprot(gene_id)
+            if not longest_seq:
+                return None
+            return longest_seq
+
+    def get_human_protein_from_uniprot_by_gene_name(self, human_gene_name):
+        human_gene_id = Ensembl.get_human_gene_id_by_gene_name(human_gene_name)
+        if not human_gene_id:
+            print("Human gene id for", human_gene_name, "cannot be found")
+            return None
+        else:
+            return self.get_human_protein_from_uniprot_by_gene_id(human_gene_id)
 
 
--- Test/Utils/BioMartTest.py
+++ Test/Utils/BioMartTest.py
@@ -1,17 +1,38 @@
 from Code.Files.FileReader import FileReader
+from Code.Http.HttpRequester import HttpRequester
 from Code.Utils.BioMart import BioMart
 
 
 class BioMartTest:
 
     @staticmethod
-    def test_get_uniprot_swissprot_id():
+    def test_get_swissprot_sequence_from_biomart():
         human_gene_ids = FileReader.get_human_genes_ids()
         bm = BioMart()
         for gene_id in human_gene_ids:
-            uni_id = bm.get_uniport_swissport_id(gene_id)
-            print("for gene id:", gene_id, "uniport_swissport id:", uni_id)
+            uni_seq = bm.get_swissprot_sequence_from_biomart(gene_id)
+            print("for gene id:", gene_id, "uniport_swissport seq:", uni_seq)
 
+    @staticmethod
+    def test_uniprot_ids():
+        # checks if there are uniprotswissprot ids that won't be recognized as those in the gene's uniprot page
+        human_gene_ids = FileReader.get_human_genes_ids()
+        bm = BioMart()
+        reviewed = 0
+        for gene_id in human_gene_ids:
+            reviewed_html = HttpRequester.get_uniprot_html(gene_id)
+            uni_ids = bm.df.loc[bm.df['Gene stable ID'] == gene_id]['UniProtKB/Swiss-Prot ID']
+            if uni_ids.size == 1:
+                uni_id = uni_ids.iloc[0]
+                if uni_id not in reviewed_html:
+                    print("id:", uni_id, "for gene:", gene_id, "doesn't seen as reviewed")
+                else:
+                    reviewed += 1
+                    if not reviewed % 1000:
+                        print(reviewed, "are good")
 ### TESTING ###
+if False:
+    BioMartTest.test_get_swissprot_sequence_from_biomart()
 
-# BioMartTest.test_get_uniprot_swissprot_id()
+if False:
+    BioMartTest.test_uniprot_ids()
