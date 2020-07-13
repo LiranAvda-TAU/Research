b4c284f7741445904eae88943f1d0f935bdad118
Liran Avda <Liranavda@gmail.com>
2020-07-13 14:38:43 +0300

    added fields to Result

--- Code/CRISPR/NamedTuples/Result.py
+++ Code/CRISPR/NamedTuples/Result.py
@@ -1,13 +1,12 @@
 from recordtype import recordtype
 
 Result = recordtype('Result', [('success', False),
+                               ('enzymes', None),
                                ('crRNA', None),
-                               ('crRNA_strand', None),
+                               ('crRNA_strand', 0),
                                ('pam_sites', None),
                                ('ssODN_strand', 0),
                                ('no_extra_inserted_mutations_sites', []),
                                ('inserted_sites', []),
-                               ('inserted_favourite', False),
                                ('no_extra_removed_mutations_sites', []),
-                               ('removed_sites', []),
-                               ('removed_favourite', False)])
+                               ('removed_sites', [])])
